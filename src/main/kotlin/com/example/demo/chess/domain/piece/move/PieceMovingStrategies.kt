bf8a97bf130f5f865c040431720d8359647ad0eb
ddaaac <skyblue300a@gmail.com>
2021-03-12 17:10:07 +0900

    feat: AttackMoving 전략 구현

--- src/main/kotlin/com/example/demo/chess/domain/piece/move/PieceMovingStrategies.kt
+++ src/main/kotlin/com/example/demo/chess/domain/piece/move/PieceMovingStrategies.kt
@@ -14,3 +14,10 @@ class JustMoving : AbstractPieceMovingStrategy() {
         return pieces.last().isEmpty()
     }
 }
+
+class AttackMoving : AbstractPieceMovingStrategy() {
+    override fun canMove(pieces: List<ChessPieceInGame>): Boolean {
+        val (source, destination) = pieces.run { first() to last() }
+        return source.isOpponentOf(destination)
+    }
+}
